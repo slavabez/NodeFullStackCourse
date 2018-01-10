7190fc88ad5797dd87ce3621b2ba6d38a068cff1
Slava Bezgachev <bezgachev@gmail.com>
2018-01-10 20:22:04 +0600

    Heroku compatible

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 const express = require('express');
 const app = express();
 
+app.get('/', (req, res) => {
+    res.send({text: 'Hello world'});
 
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT);
+console.log(`Application listening on port ${PORT}`);
