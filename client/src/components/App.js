3e10d69062ba3d42686858bc8e70337dd51b9417
Slava Bezgachev <bezgachev@gmail.com>
2018-01-14 01:32:40 +0600

    Working on adding redux

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,5 +1,8 @@
 import React, {Component} from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
+import { connect } from 'react-redux';
+import * as actions from '../actions';
+
 import Header from './Header';
 
 const Dashboard= () => <h2>Dashboard</h2>;
@@ -7,6 +10,10 @@ const SurveyNew = () => <h2>Survey New</h2>;
 const Landing = () => <h2>Landing</h2>;
 
 class App extends Component {
+
+    componentDidMount(){
+        this.props.fetchUser();
+    }
 
     render(){
         return (<div className="container">
@@ -24,4 +31,4 @@ class App extends Component {
 
 }
 
-export default App;
+export default connect(null, actions)(App);
